cf1578dc13d3b2bf470afcc260cc456961c37eb3
Khurram <youthpassion.com@gmail.com>
2023-03-29 17:17:02 +0500

    new changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FacultyComponent } from './components/faculty/faculty/add-teacher/faculty.component';
-import { AllFacultyComponent } from './components/faculty/faculty/all-faculty/all-faculty.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
-import { AddStudentComponent } from './components/student/add-student/add-student.component';
-import { AllStudentComponent } from './components/student/all-student/all-student.component';
 import { AddNewGiveAndTakeComponent } from './pages/add-new-give-and-take/add-new-give-and-take.component';
 import { AddPartieComponent } from './pages/add-partie/add-partie.component';
 import { PartiesComponent } from './pages/parties/parties.component';
@@ -15,16 +11,12 @@ import { AuthenticationGuard } from './services/authentication.guard';
 
 const routes: Routes = [
   {path: 'login',component: LoginComponent},
-  // {path: 'sign-up',component: SignupComponent},
+  {path: 'sign-up',component: SignupComponent},
   {path: 'all-parties',component: PartiesComponent, canActivate: [AuthenticationGuard]},
   {path: 'add-partie',component: AddPartieComponent, canActivate: [AuthenticationGuard]},
   {path: 'partie-detail',component: ViewPartieDetailComponent, canActivate: [AuthenticationGuard]},
   {path: 'add-new-giveAndTake',component: AddNewGiveAndTakeComponent, canActivate: [AuthenticationGuard]},
   {path: 'add-role',component: UserRoleComponent, canActivate: [AuthenticationGuard]},
-  // {path: 'add-student',component: AddStudentComponent, canActivate: [AuthenticationGuard]},
-  // {path: 'student-record',component: AllStudentComponent, canActivate: [AuthenticationGuard]},
-  // {path: 'add-faculty',component: FacultyComponent, canActivate: [AuthenticationGuard]},
-  // {path: 'faculty-record',component: AllFacultyComponent, canActivate: [AuthenticationGuard]},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { HeaderComponent } from './components/header/header.component';
-import { AllFacultyComponent } from './components/faculty/faculty/all-faculty/all-faculty.component';
-import { AddStudentComponent } from './components/student/add-student/add-student.component';
-import { FacultyComponent } from './components/faculty/faculty/add-teacher/faculty.component';
-import { AllStudentComponent } from './components/student/all-student/all-student.component';
 import { LoginComponent } from './components/login/login.component';
 import { AuthGuradService } from './services/auth-gurad-service';
 import { SignupComponent } from './components/signup/signup.component';
@@ -25,11 +21,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent,
-    AddStudentComponent,
     HeaderComponent,
-    FacultyComponent,
-    AllStudentComponent,
-    AllFacultyComponent,
     LoginComponent,
     SignupComponent,
     PartiesComponent,
